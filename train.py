8c95bf21f2135c0bba7bd050722425324755761e
bbc1335 <67258869+bbc1335@users.noreply.github.com>
2025-02-25 21:53:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("ultralytics/cfg/models/v8/TLFINet-Ablation-Focus.yaml")  # build a new model from scratch
-# model = YOLO("yolov8m.yaml")  # build a new model from scratch
-# model = YOLO("/home/bbc1335/Documents/Detection/ultralytics/runs/detect/YOLOv8改3/weights/best.pt")  # Load a pretrained model (recommended for training)
+model = YOLO("ultralytics/cfg/models/v8/TLFINet.yaml")  # build a new model from scratch
 
 # Use the model
 model.train(data="NEU-DET.yaml",
@@ -12,11 +10,4 @@ model.train(data="NEU-DET.yaml",
             imgsz=224,
             patience=100,
             workers=8,
-            # optimizer="SGD",
-            # conf=0.001,
-            # iou=0.5,
-            name="AblationTLFINetFocuse300sz224",
             close_mosaic=15)
-# metrics = model.val()  # evaluate model performance on the validation set
-# results = model("")  # predict on an image
-# path = model.export(format="onnx")  # export the model to ONNX format
